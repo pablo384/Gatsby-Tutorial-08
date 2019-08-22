adaa8437b32c1585e14cf324baa6aa6fc64adced
Pablo384 <pabloreinoso0@gmail.com>
2019-08-22 12:27:23 -0400

    Programmatically create pages from data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
-
+import { Link, graphql } from "gatsby"
 export default ({ data }) => {
     console.log(data)
     return (
@@ -20,21 +19,29 @@ export default ({ data }) => {
                 <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
                 {data.allMarkdownRemark.edges.map(({ node }) => (
                     <div key={node.id}>
-                        <h3
+                        <Link
+                            to={node.fields.slug}
                             css={css`
-                margin-bottom: ${rhythm(1 / 4)};
+                text-decoration: none;
+                color: inherit;
               `}
                         >
-                            {node.frontmatter.title}{" "}
-                            <span
+                            <h3
                                 css={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+                            >
+                                {node.frontmatter.title}{" "}
+                                <span
+                                    css={css`
                   color: #bbb;
                 `}
-                            >
-                                — {node.frontmatter.date}
-                            </span>
-                        </h3>
-                        <p>{node.excerpt}</p>
+                                >
+                                    — {node.frontmatter.date}
+                                </span>
+                            </h3>
+                            <p>{node.excerpt}</p>
+                        </Link>
                     </div>
                 ))}
             </div>
@@ -44,18 +51,21 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark (sort: { fields: [frontmatter___date], order: DESC }){
-      totalCount
+                    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC }){
+                    totalCount
       edges {
-        node {
-          id
+                    node {
+                id
           frontmatter {
-            title
+                    title
             date(formatString: "DD MMMM, YYYY")
+              }
+              fields {
+            slug
           }
-          excerpt
+              excerpt
+            }
+          }
         }
       }
-    }
-  }
 `
