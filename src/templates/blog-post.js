52dd67207c53f216b5a46b571a7ab30cd95e7156
Pablo384 <pabloreinoso0@gmail.com>
2019-08-22 12:38:13 -0400

    Preparing a site to go live

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 export default ({ data }) => {
     const post = data.markdownRemark
     return (
         <Layout>
             <div>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>{post.frontmatter.title}</title>
+                    <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
                 <h1>{post.frontmatter.title}</h1>
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
             </div>
